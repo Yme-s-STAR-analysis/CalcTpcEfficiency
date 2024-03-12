3d5115ea4bdc3bb67232c6e0bab9f6b7e87ed854
Yme <yghuang@mails.ccnu.edu.cn>
2024-03-12 12:34:13 +0100

    v5.0 New main function to support new interfaces

--- main.cxx
+++ main.cxx
@@ -1,11 +1,24 @@
+#include <iostream>
 #include "EffTool.h"
 
+using std::cout;
+using std::endl;
+
 int main(int argc, char* argv[]){
-    TpcEffMaker* maker = new TpcEffMaker(argv[1], argv[2], argv[3]);
-    maker->set_cut_dca(1);
-    maker->set_cut_nHitsFit(20);
-    maker->make(0); // proton
-    maker->make(1); // antiproton
+    cout << "[LOG] Welcome to TPC tracking efficiency histogram generator!" << endl;
+    cout << "[LOG] Arguments:" << endl;
+    cout << "[LOG]  -> File list fDst tree: " << argv[1] << endl;
+    cout << "[LOG]  -> Proton type: " << argv[2] << endl;
+    cout << "[LOG]  -> Output file name: " << argv[3] << endl;
+    cout << "[LOG]  -> DCA cut " << argv[4] << endl;
+    cout << "[LOG]  -> nHitsFit " << argv[5] << endl;
+    int mode = atoi(argv[2]);
+    TpcEffMaker* maker = new TpcEffMaker(argv[3]);
+    double mDcaCut = atof(argv[4]);
+    double mNhitsCut = atoi(argv[5]);
+    maker->set_cut_dca(mDcaCut);
+    maker->set_cut_nHitsFit(mNhitsCut);
+    maker->make(argv[1], mode); 
     maker->write();
     return 0;
 }
