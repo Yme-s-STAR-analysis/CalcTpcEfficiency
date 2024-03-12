589fc846daf0a16d3c30791bbbd82036762d8659
Yme <yghuang@mails.ccnu.edu.cn>
2024-03-12 12:33:45 +0100

    v5.0 Local test

--- do.sh
+++ do.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-./run proton.root antiproton.root TpcEff.root
+# v4.0-
+# ./run proton.root antiproton.root TpcEff.root
+
+# v5.0+
+./run proton.list 0 TpcEffRawPro.root 1.0 20
+./run antiproton.list 1 TpcEffRawPbar.root 1.0 20
